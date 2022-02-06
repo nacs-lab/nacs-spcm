3b563287661b1755d7eccbbd36c69ba4ff147f21
Yichao Yu <yyc1992@gmail.com>
2022-02-06 09:26:17 -0500

    Use round function from libnacs

--- nacs-spcm/data_stream_p.h
+++ nacs-spcm/data_stream_p.h
@@ -69,13 +69,7 @@ namespace scalar {
 // of doing it once per channel.
 static NACS_INLINE float sinpif_pi(float d)
 {
-#if NACS_CPU_X86 || NACS_CPU_X86_64
-    int q = _mm_cvtss_si32(_mm_set_ss(d));
-#elif NACS_CPU_AARCH64
-    int q = vcvtns_s32_f32(d);
-#else
-    int q = d < 0 ? (int)(d - 0.5f) : (int)(d + 0.5f);
-#endif
+    int q = round<int>(d);
     // Now `d` is the fractional part in the range `[-0.5, 0.5]`
     d = d - (float)q;
     auto s = d * d;
