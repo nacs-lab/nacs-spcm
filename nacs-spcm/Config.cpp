b05e4dd84da69770620f90b42596aa1dbe410e13
Ni Lab <niharvardlab@gmail.com>
2025-03-18 19:45:45 +0000

    simple syntax error

--- nacs-spcm/Config.cpp
+++ nacs-spcm/Config.cpp
@@ -22,9 +22,10 @@ NACS_EXPORT() Config Config::loadYAML(const char *fname)
         conf.amp = amp_node.as<uint32_t>();
     if (auto sample_rate_node = file["sample_rate_mhz"])
         conf.sample_rate = sample_rate_node.as<int64_t>() * 1e6;
-    if (auto trig_node = file["trig_delay_ms"])
+    if (auto trig_node = file["trig_delay_ms"]) {
         conf.trig_delay = uint64_t(trig_node.as<double>() * conf.sample_rate / (32 * 1e3)); // units of stream times
         conf.trig_delay_ms = uint64_t(trig_node.as<double>()); // record actual trig delay in ms
+    }
     if (auto clock_node = file["clock_cycle_factor"])
         conf.ext_clock_cycle_factor = uint64_t(clock_node.as<double>());
     if (conf.sample_rate  > 71.68e6) {
